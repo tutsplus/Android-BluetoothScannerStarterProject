e6c9c165c875de20b5879c1077ebc6ab9d166ddc
Bart Jacobs <developer@bartjacobs.com>
2015-06-15 05:09:05 +0200

    Initial commit

---
+++ BluetoothScanner/app/src/main/java/com/tutsplus/matt/bluetoothscanner/ListActivity.java
@@ -0,0 +1,62 @@
+package com.tutsplus.matt.bluetoothscanner;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ListActivity extends ActionBarActivity implements DeviceListFragment.OnFragmentInteractionListener  {
+
+
+    private DeviceListFragment mDeviceListFragment;
+    private BluetoothAdapter BTAdapter;
+
+
+    public static int REQUEST_BLUETOOTH = 1;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //TODO Check if bluetooth is enabled
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mDeviceListFragment = DeviceListFragment.newInstance(BTAdapter);
+        fragmentManager.beginTransaction().replace(R.id.container, mDeviceListFragment).commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+}
